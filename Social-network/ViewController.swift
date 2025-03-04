82f601f594c46ae1e1d3ca01a4ff871abfc084a3
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-03-04 12:20:31 +0300

    Initial Commit

---
+++ Social-network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social-network
+//
+//  Created by Даша Николаева on 04.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
