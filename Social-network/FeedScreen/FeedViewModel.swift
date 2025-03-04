c2a90ae6a3fbbe586c312f885739776da7f8437e
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-03-04 15:29:16 +0300

    initial commit, add network service

---
+++ Social-network/FeedScreen/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  Social-network
+//
+//  Created by Даша Николаева on 04.03.2025.
+//
+
+import Foundation
+
+class FeedViewModel {
+    
+}
---
+++ Social-network/NetworkService.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkService.swift
+//  Social-network
+//
+//  Created by Даша Николаева on 04.03.2025.
+//
+
+import Alamofire
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func fetchData<T: Decodable>(url: String, completion: @escaping (Result<T, Error>) -> ())
+    func fetchImage(url: String, completion: @escaping (Result<Data, Error>) -> ())
+}
+
+class NetworkService {
+    static let shared = NetworkService(); private init() {}
+    
+    func fetchData<T: Decodable>(url: String, completion: @escaping (Result<T, Error>) -> ()) {
+        AF.request(url).validate().responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let value):
+                completion(.success(value))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+            
+        }
+    }
+    
+    func fetchImage(url: String, completion: @escaping (Result<Data, Error>) -> ()) {
+        AF.request(url).validate().responseData { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
